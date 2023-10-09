f1a99e41ed791a127da72e3b7fd180ffc93e66d1
Lyon <lyonforwork@yeah.net>
2023-10-10 00:04:37 +0800

    Added frontend UI

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,22 +1,28 @@
 const asyncHandler = require('express-async-handler');
-const Goal = require('../models/goalModels')
+const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 const {text} = require("express");
+
+
 // @desc   Get Goals
 // @routes GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-	const goals = await Goal.find();
+	const goals = await Goal.find({user: req.user.id});
 	res.status(200).json(goals)
 })
+
 // @desc   Set Goal
 // @routes POST /api/goals
 // @access Private
 const setGoal = asyncHandler(async (req, res) => {
 	const goals = await Goal.create({
-		text: req.body.text
+		text: req.body.text,
+		user: req.user.id
 	})
 	res.status(200).json(goals)
 })
+
 // @desc   Update Goal
 // @routes PUT /api/goals/:id
 // @access Private
@@ -26,6 +32,20 @@ const updateGoal = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('Goal not found');
 	}
+	
+	const user = await User.findById(req.user.id)
+	
+	if(!user){
+		res.status(401)
+		throw new Error('User not found')
+	}
+	
+	// Make sure the logged in user matches th goal user
+	if(goal.user.toString() !== user.id){
+		res.status(401)
+		throw new Error('User not authorized')
+	}
+	
 	const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 	})
@@ -33,14 +53,30 @@ const updateGoal = asyncHandler(async (req, res) => {
 		message: `update msg${req.params.id}`
 	})
 })
+
 // @desc   Delete Goal
 // @routes DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
+	const goal = await Goal.findById(req.params.id);
 	if (!Goal.findById(req.params.id)){
 		res.status(400);
 		throw new Error('Goal not found');
 	}
+		
+	const user = await User.findById(req.user.id)
+	
+	if(!user){
+		res.status(401)
+		throw new Error('User not found')
+	}
+	
+	// Make sure the logged in user matches th goal user
+	if(goal.user.toString() !== user.id){
+		res.status(401)
+		throw new Error('User not authorized')
+	}
+	
 	await Goal.deleteOne({_id : req.params.id})
 	res.status(200).json({
 		message: `delete msg${req.params.id}`
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {FaSignInAlt, FaUser} from "react-icons/fa";
+
+function Login() {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	})
+	
+	const {name, email, password, password2} = formData
+	
+	const onChange = () => {
+	
+	}
+	
+	const onSubmit = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value
+		}))
+	}
+	
+	
+	return (
+		<>
+			<section className="heading">
+				<h1>
+					<FaSignInAlt />Login
+				</h1>
+				<p>Login and start setting goals</p>
+			</section>
+			
+			<sectino className="form">
+				<form>
+					<div className="form-group">
+						<input
+							type="email"
+							className='form-control'
+							id='email'
+							name='email'
+							value={email}
+							placeholder='Enter your email'
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<input
+							type="password"
+							className='form-control'
+							id='password'
+							name='password'
+							value={password}
+							placeholder='Enter your password'
+							onChange={onChange}
+						/>
+					</div>
+					<div className="form-group">
+						<button type='submit' className="btn btn-block">
+							Submit
+						</button>
+					</div>
+				</form>
+			</sectino>
+		</>
+	);
+}
+
+export default Login;
